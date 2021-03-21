9d12f88a5a39328f79579759af9ba92e44ccd8b3
syd333 <sydmiranda@icloud.com>
2021-03-20 17:01:52 -0700

    commit tree 64bd5b4824e66aa128788d001e5228d04a9b7f0b

--- src/App.js
+++ src/App.js
@@ -1,1 +1,22 @@
+// const { Component } = require("react")
+
 // create your App component here
+import React, {Component} from 'react'
+class App extends Component {
+
+    componentDidMount(){
+        fetch('http://api.open-notify.org/astros.json')
+        .then(res => res.json())
+        .then(data => {
+            console.log
+        })
+    }
+
+    render(){
+        return(
+            <div></div>
+        )
+    }
+}
+
+export default App;
